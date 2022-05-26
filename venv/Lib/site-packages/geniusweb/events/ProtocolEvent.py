2cac0c257e8fb505bfa0c9da3a5a5eec322001cd
Joe Shymanski <jshyman517@gmail.com>
2022-05-26 13:49:31 -0500

    Deleted venv folder, added it to .gitignore

--- venv/Lib/site-packages/geniusweb/events/ProtocolEvent.py
+++
@@ -1,9 +0,0 @@
-from geniusweb.events.AbstractEvent import AbstractEvent
-
-class ProtocolEvent (AbstractEvent):
-	'''
-	an event triggered by the protocol.
-	'''
-	
-	def ProtocolEvent(self, now:int):
-		super().__init__(now)
--- venv/Lib/site-packages/geniusweb/protocol/session/SessionProtocol.py
+++
@@ -1,9 +0,0 @@
-from pyson.JsonSubTypes import JsonSubTypes
-from geniusweb.protocol.NegoProtocol import NegoProtocol
-
-@JsonSubTypes([ "SAOP" ])
-class SessionProtocol (NegoProtocol):
-    '''
-    Interface for all session protocols.
-    '''
-
